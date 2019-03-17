16c5460b586f5a74a4b33c14cdb243a86ceb4fdb
Moskalenko Sergey <oknelaksoms@gmail.com>
2019-03-17 18:02:55 +0200

    first

---
+++ Speed/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Speed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.glControl1 = new OpenTK.GLControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(12, 12);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(876, 876);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.glControl1_KeyDown);
+            this.glControl1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.glControl1_KeyPress);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(900, 900);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Speed/Form1.cs
@@ -0,0 +1,236 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Math;
+using OpenTK.Platform.Windows;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Speed
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //int texture;
+        int x, y, z;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1;
+            timer1.Enabled = true;
+            //timer2.Interval = 1;
+            //timer2.Enabled = true;
+            //glControl1.SwapBuffers();
+            //glControl1.Width = glControl1.Height;
+        }
+
+        //public static int load_texture(string path)
+        //{
+        //int id = GL.GenTexture();
+        //GL.BindTexture(TextureTarget.Texture2D, id);
+        //Bitmap bmp = new Bitmap(path);
+        //BitmapData data = bmp.LockBits(new Rectangle(0,0,bmp.Width,bmp.Height),ImageLockMode.ReadOnly,System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        //GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+        //bmp.UnlockBits(data);
+        ////GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Clamp);
+        ////GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Clamp);
+        //GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+        ////GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+        //return id;
+        //}
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {//текстура!!!
+         /*GL.Enable(EnableCap.Texture2D);
+         texture = load_texture(@"C:\Users\moskalenko_s\Desktop\My games\Speed\Speed\House01.png");
+         GL.Clear(ClearBufferMask.ColorBufferBit);
+         GL.ClearColor(Color.SandyBrown);
+
+         GL.BindTexture(TextureTarget.Texture2D, texture);
+         GL.Begin(PrimitiveType.Quads);
+         GL.Color3(Color.Brown);
+         GL.TexCoord2(0, 0);
+         GL.Vertex2(0, 0);
+
+         GL.TexCoord2(1, 0);
+         GL.Vertex2(0.4, 0);
+
+         GL.TexCoord2(1, 1);
+         GL.Vertex2(0.4, -0.4);
+
+         GL.TexCoord2(0, 1);
+         GL.Vertex2(0, -0.4);
+
+         GL.End();
+
+
+         glControl1.SwapBuffers();*/
+
+            GL.ClearColor(Color.White);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            Bitmap bitmap = new Bitmap(@"C:\Users\moskalenko_s\Desktop\My games\Speed\Speed\House01.png");
+            int texture;
+
+            
+            GL.Enable(EnableCap.Texture2D);
+
+            GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+
+            GL.GenTextures(1, out texture);
+            GL.BindTexture(TextureTarget.Texture2D, texture);
+
+            BitmapData data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+            bitmap.UnlockBits(data);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            
+
+            
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.BindTexture(TextureTarget.Texture2D, texture);
+            for (double i = -1; i < 1; i += 0.1)
+                for (double j = -1; j < 1; j += 0.1)
+                {
+                    GL.Begin(BeginMode.Quads);
+                    GL.TexCoord2(0.0, 0.0); GL.Vertex2(i, j);
+                    GL.TexCoord2(-1.0, 0.0); GL.Vertex2(i + 0.1, j);
+                    GL.TexCoord2(-1.0, -1.0); GL.Vertex2(i + 0.1, j + 0.1);
+                    GL.TexCoord2(0.0, -1.0); GL.Vertex2(i, j + 0.1);
+                    GL.End();
+                }
+
+            glControl1.SwapBuffers();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.ClearColor(Color.Black);
+            GL.Begin(PrimitiveType.Lines);
+            Cube();
+            GL.End();
+            if (x != 0 || y != 0 || z != 0)
+            {
+                GL.Begin(PrimitiveType.Patches);
+                GL.Rotate(1, x, y, z);
+                GL.End();
+            }
+            glControl1.SwapBuffers();
+        }
+
+        private void glControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //label1.Text = e.KeyValue.ToString();
+        }
+
+        private void glControl1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.CompareTo('a') == 0)
+            {
+                x++;
+            }
+            else if (e.KeyChar.CompareTo('d') == 0)
+            {
+                x--;
+            }
+            if (e.KeyChar.CompareTo('w') == 0)
+            {
+                y++;
+            }
+            else if (e.KeyChar.CompareTo('s') == 0)
+            {
+                y--;
+            }
+            if (e.KeyChar.CompareTo('x') == 0)
+            {
+                z++;
+            }
+            else if (e.KeyChar.CompareTo('z') == 0)
+            {
+                z--;
+            }
+            if (e.KeyChar.CompareTo(' ') == 0)
+            {
+                y = 0;
+                x = 0;
+                z = 0;
+            }
+            label1.Text = x.ToString() + " " + y.ToString() + " " + z.ToString();
+        }
+
+        void Cube()
+        {
+            GL.Color3(Color.Red);
+            GL.Vertex3(0.5, 0.5, 0.5);
+            GL.Vertex3(-0.5, 0.5, 0.5);
+
+            GL.Vertex3(-0.5, 0.5, 0.5);
+            GL.Vertex3(-0.5, 0.5, -0.5);
+
+            GL.Vertex3(-0.5, 0.5, -0.5);
+            GL.Vertex3(0.5, 0.5, -0.5);
+
+            GL.Vertex3(0.5, 0.5, -0.5);
+            GL.Vertex3(0.5, 0.5, 0.5);
+
+            GL.Color3(Color.Yellow);
+            GL.Vertex3(0.5, -0.5, 0.5);
+            GL.Vertex3(0.5, -0.5, -0.5);
+
+            GL.Vertex3(0.5, -0.5, -0.5);
+            GL.Vertex3(-0.5, -0.5, -0.5);
+
+            GL.Vertex3(-0.5, -0.5, -0.5);
+            GL.Vertex3(-0.5, -0.5, 0.5);
+
+            GL.Vertex3(-0.5, -0.5, 0.5);
+            GL.Vertex3(0.5, -0.5, 0.5);
+
+            GL.Color3(Color.Blue);
+            GL.Vertex3(0.5, 0.5, 0.5);
+            GL.Vertex3(0.5, -0.5, 0.5);
+
+            GL.Vertex3(-0.5, 0.5, 0.5);
+            GL.Vertex3(-0.5, -0.5, 0.5);
+
+            GL.Vertex3(-0.5, 0.5, -0.5);
+            GL.Vertex3(-0.5, -0.5, -0.5);
+
+            GL.Vertex3(0.5, 0.5, -0.5);
+            GL.Vertex3(0.5, -0.5, -0.5);
+
+            GL.Color3(Color.Green);
+            GL.Vertex3(0.5, 0.5, 0.5);
+            GL.Vertex3(-0.5, -0.5, -0.5);
+
+            GL.Vertex3(-0.5, 0.5, 0.5);
+            GL.Vertex3(0.5, -0.5, -0.5);
+
+            GL.Vertex3(-0.5, 0.5, -0.5);
+            GL.Vertex3(0.5, -0.5, 0.5);
+
+            GL.Vertex3(0.5, 0.5, -0.5);
+            GL.Vertex3(-0.5, -0.5, 0.5);
+
+        }
+    }
+}
